ae411d01627b3edf591afeed18afdde5cb05b153
DWaltenburg <113024423+DWaltenburg@users.noreply.github.com>
2022-09-07 13:00:53 +0200

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,58 @@
+import threading
+import socket
+import sys
+import time
+
+
+host = ''
+port = 12345
+locaddr = (host,port)
+
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+tello_address = ('172.31.146.23', 9000)
+
+sock.bind(locaddr)
+
+def recv():
+    count = 0
+    while True:
+        try:
+            data, server = sock.recvfrom(1518)
+            print(data.decode(encoding="utf-8"))
+        except Exception:
+            print ('\nExit . . .\n')
+            break
+
+
+print ('\r\n\r\nTello Python3 Demo.\r\n')
+
+print ('Tello: command speed?\r\n')
+
+print ('end -- quit demo.\r\n')
+
+
+#recvThread create
+recvThread = threading.Thread(target=recv)
+recvThread.start()
+
+while True:
+    try:
+        msg = input("");
+        if not msg:
+            break
+
+        if 'end' in msg:
+            print ('...')
+            sock.close()
+            break
+
+        # Send data
+        msg = msg.encode(encoding="utf-8")
+        sent = sock.sendto(msg, tello_address)
+    except KeyboardInterrupt:
+        print ('\n . . .\n')
+        sock.close()
+        break
